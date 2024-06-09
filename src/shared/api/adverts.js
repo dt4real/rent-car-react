53e66a7e6bc4b5dc3ef230c6506c41bad2c34465
dt4real <dotchere95@gmail.com>
2024-06-09 22:25:21 +0200

    Update Vite config for GitHub Pages

--- src/shared/api/adverts.js
+++ src/shared/api/adverts.js
@@ -3,9 +3,9 @@ import { instance } from './instance';
 export const getAllAdverts = async () => {
   try {
     const { data } = await instance.get('/');
-    console.log(data);
     return data;
   } catch ({ response }) {
+    // eslint-disable-next-line no-console
     console.log(response.data);
   }
 };
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/rent-car-react/',
-  plugins: [react()]
+  plugins: [react()],
+  base: '/rent-car-react',
+  resolve: {
+    alias: { src: '/src' }
+  }
 });
